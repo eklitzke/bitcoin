25f71ef1222259fc1e65711e3a54bd23796d9235
Evan Klitzke <evan@eklitzke.org>
2018-03-16 11:07:39 -0700

    .

--- test/functional/feature_logging.py
+++ test/functional/feature_logging.py
@@ -58,11 +58,10 @@ class LoggingTest(BitcoinTestFramework):
         assert os.path.isfile(os.path.join(invdir, "foo.log"))
 
         # check that -nodebuglog disables logging
-        log_path = self.relative_log_path("debug.log")
-        os.unlink(log_path)
+        log_path = self.relative_log_path("nosuch.log")
         assert not os.path.isfile(log_path)
         self.stop_node(0)
-        self.start_node(0, ["-nodebuglog"])
+        self.start_node(0, ["-nodebuglog", "-debuglogfile=nosuch.log"])
         assert not os.path.isfile(log_path)
 
         # just sanity check no crash here
